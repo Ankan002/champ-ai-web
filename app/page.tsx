aeee4c1bda42fb89bc15c7d33e336cf7c17a9458
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-06-30 00:38:34 +0530

    feat: :sparkles: add shadcn-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return (
-    <main className="min-h-screen w-full bg-white">
-		<h1 className="text-black">Hello</h1>
-	</main>
-  )
+	return (
+		<main className="min-h-screen w-full bg-white">
+			<h1 className="text-black">Hello</h1>
+		</main>
+	);
 }
