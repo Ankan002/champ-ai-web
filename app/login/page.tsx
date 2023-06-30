af59716f4d6029902f6e499e1577aebf4b1df918
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-06-30 17:44:13 +0530

    feat: :sparkles: add base components

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,31 @@
-const LoginPage = () => {
-    return (
-        <main className="w-full min-h-screen flex flex-col p-5 bg-pink-100">
-            <div className="flex-1 flex bg-yellow-500">
+import LoginHero from "@/assets/images/login-hero.jpg";
+import LoginKeyImage from "@/assets/images/login-key.png";
+import Image from "next/image";
 
-            </div>
-        </main>
-    )
+const LoginPage = () => {
+	return (
+		<main className="w-full min-h-screen flex flex-col lg:bg-gradient-to-r lg:from-secondary-yellow  lg:to-primary-light bg-primary-light">
+			<div className="w-full h-screen flex items-center">
+				<Image
+					src={LoginHero.src}
+					height={1568}
+					width={960}
+					alt=""
+					className="h-full w-7/12 object-cover rounded-r-xl border-r-2 border-t-2 border-b-2 border-secondary-dark lg:block hidden"
+				/>
+				<div className="flex-1 h-screen flex flex-col">
+					<div></div>
+					<div className="flex-1 w-full flex flex-col items-center justify-center p-5">
+						<Image src={LoginKeyImage.src} height={460} width={778} alt="login key" className="w-72 h-64 object-contain" />
+
+						<p className="lg:text-xl md:text-lg text-base font-quicksand text-center font-semibold tracking-widest mt-5 lg:w-full w-2/3">
+							{"Let's"} embrace the magic and power of knowledge with the help of Artificial Intelligence
+						</p>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default LoginPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,27 @@ module.exports = {
 				manrope: "'Manrope', sans-serif",
 				"fira-code": "'Fira Code', monospace",
 			},
+			colors: {
+				"primary-dark": "#10111A",
+				"secondary-dark": "#08070C",
+				"primary-light": "#F4F6FA",
+				"secondary-light": "#F1F2F6",
+				"primary-orange": "#FC5830",
+				"secondary-orange": "#FF0032",
+				"light-grey": "#B5BAC7",
+				white: "#FFFFFF",
+				"primary-yellow": "#F6BA3D",
+				"secondary-yellow": "#FEE9C0",
+				"semi-transparent": "rgba(0, 0, 0, 0.6)",
+				"primary-green": "#08CD92",
+				"secondary-green": "#C1FBE6",
+				"dark-green": "#51AB39",
+				"primary-red": "#D61C4E",
+				"primary-blue": "#59AFEF",
+				"primary-purple": "#E7C7FF",
+				"secondary-purple": "#8F30E0",
+				//"login-image-hero-base-color": "#8559FF"
+			},
 			keyframes: {
 				"accordion-down": {
 					from: { height: 0 },
