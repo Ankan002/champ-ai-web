19e0ac11f5c2c328dc0468d7d566aa7518126f5a
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-06-30 18:08:04 +0530

    chore: :wrench: update meta-data info

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -16,10 +16,20 @@ const LoginPage = () => {
 				<div className="flex-1 h-screen flex flex-col">
 					<div></div>
 					<div className="flex-1 w-full flex flex-col items-center justify-center p-5">
-						<Image src={LoginKeyImage.src} height={460} width={778} alt="login key" className="w-72 h-64 object-contain" />
+						<Image
+							src={LoginKeyImage.src}
+							height={460}
+							width={778}
+							alt="login key"
+							className="w-72 h-64 object-contain"
+						/>
 
-						<p className="lg:text-xl md:text-lg text-base font-quicksand text-center font-semibold tracking-widest mt-5 lg:w-full w-2/3">
-							{"Let's"} embrace the magic and power of knowledge with the help of Artificial Intelligence
+						<h1 className="font-quicksand text-4xl font-bold tracking-widest mt-6 text-center">
+							WELCOME !
+						</h1>
+
+						<p className="text-xl font-quicksand text-center tracking-widest mt-5 lg:w-full sm:w-2/3 w-full">
+							{"Let's"} embrace the magic and power of knowledge
 						</p>
 					</div>
 				</div>
