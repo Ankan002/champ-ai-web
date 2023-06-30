3b1db4b87716601e2d7f9cb518c87ad60602b760
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-06-30 10:40:44 +0530

    feat: :sparkles: add font family

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<main className="min-h-screen w-full bg-white">
-			<h1 className="text-black">Hello</h1>
+			<h1 className="text-black text-3xl font-quicksand">Hello</h1>
 		</main>
 	);
 }
