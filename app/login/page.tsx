6e4d2430e7b903b600ad3b4c001e463d8cfbf671
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-06-30 12:49:17 +0530

    feat: :sparkles: add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+const LoginPage = () => {
+    return (
+        <main className="w-full min-h-screen flex flex-col p-5 bg-pink-100">
+            <div className="flex-1 flex bg-yellow-500">
+
+            </div>
+        </main>
+    )
+};
+
+export default LoginPage;
