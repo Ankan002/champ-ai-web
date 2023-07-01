e2de64c7e571355fd7c028244183cb118494f0fe
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-07-01 17:42:23 +0530

    feat: :sparkles: add hero nav bar

---
+++ components/login/hero-animation-navbar/index.ts
@@ -0,0 +1,1 @@
+export { default as HeroAnimationNavbar } from "./hero-animation-navbar";
--- components/login/login-hero-animation/login-hero-animation.tsx
+++ components/login/login-hero-animation/login-hero-animation.tsx
@@ -1,15 +1,22 @@
 import { MacAnimationBtn } from "../elements";
+import { HeroAnimationNavbar } from "../hero-animation-navbar";
 
 const LoginHeroAnimation = () => {
-    return (
-        <div className="flex-1 w-full h-96 flex flex-col border-2 rounded-md border-primary-dark shadow-lg shadow-primary-dark/40 overflow-y-scroll">
-            <div className="flex justify-end px-2 py-2 border-b-2 bg-secondary-light border-b-primary-dark">
-                <MacAnimationBtn backgroundColor="bg-primary-green" />
-                <MacAnimationBtn backgroundColor="bg-primary-yellow" />
-                <MacAnimationBtn backgroundColor="bg-primary-red" />
+	return (
+		<div className="flex-1 w-full h-96 flex flex-col border-2 rounded-md border-primary-dark shadow-lg shadow-primary-dark/40">
+			<div className="flex justify-end px-2 py-2 border-b-2 bg-secondary-light rounded-md border-b-primary-dark">
+				<MacAnimationBtn backgroundColor="bg-primary-green" />
+				<MacAnimationBtn backgroundColor="bg-primary-yellow" />
+				<MacAnimationBtn backgroundColor="bg-primary-red" />
+			</div>
+
+			<HeroAnimationNavbar />
+
+            <div className="flex-1">
+
             </div>
-        </div>
-    );
+		</div>
+	);
 };
 
 export default LoginHeroAnimation;
